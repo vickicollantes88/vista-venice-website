07cdd51f3c01331e0ef41a9db60b8c11f70a2cde
vickicollantes88 <vickicollantes88@gmail.com>
2026-02-28 15:06:13 +0800

    Update script.js

--- script.js
+++ script.js
@@ -16,3 +16,26 @@ card.style.transform = "translateY(40px)";
 card.style.transition = "0.8s ease";
 observer.observe(card);
 });
+
+// ================= LIGHTBOX =================
+const galleryItems = document.querySelectorAll(".gallery-item");
+const lightbox = document.getElementById("lightbox");
+const lightboxImg = document.getElementById("lightbox-img");
+const closeLightbox = document.querySelector(".close-lightbox");
+
+galleryItems.forEach(item => {
+  item.addEventListener("click", () => {
+    lightbox.style.display = "block";
+    lightboxImg.src = item.src;
+  });
+});
+
+closeLightbox.addEventListener("click", () => {
+  lightbox.style.display = "none";
+});
+
+lightbox.addEventListener("click", (e) => {
+  if (e.target === lightbox) {
+    lightbox.style.display = "none";
+  }
+});
